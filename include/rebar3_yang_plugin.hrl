5fe51484901af51354d4850a97ca0fe7b4d1d734
Yury Gargay <yury.gargay@gmail.com>
2015-06-30 19:16:40 +0600

    Add clean target

---
+++ include/rebar3_yang_plugin.hrl
@@ -0,0 +1,7 @@
+-ifndef(YANG_PLUGIN).
+-define(YANG_PLUGIN, 1).
+
+-define(YANGDIR, filename:join(["src", "yang"])).
+-define(YANGBUILDDIR, filename:join(["src", "yang", "hrl"])).
+
+-endif.
---
+++ src/rebar3_yang_plugin_clean.erl
@@ -0,0 +1,41 @@
+-module(rebar3_yang_plugin_clean).
+-behaviour(provider).
+
+-export([init/1, do/1, format_error/1]).
+
+-define(PROVIDER, clean).
+-define(DEPS, [{default, app_discovery}]).
+
+-include_lib("providers/include/providers.hrl").
+-include("rebar3_yang_plugin.hrl").
+
+%% ===================================================================
+%% Public API
+%% ===================================================================
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    Provider = providers:create([{name,       ?PROVIDER},
+                                 {module,     ?MODULE},
+                                 {namespace,  yang},
+                                 {bare,       true},
+                                 {deps,       ?DEPS},
+                                 {example,    "rebar3 yang clean"},
+                                 {short_desc, "compile yang files."},
+                                 {desc,       "compile yang files."},
+                                 {opts,       []}]),
+    {ok, rebar_state:add_provider(State, Provider)}.
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
+do(State) ->
+    Opts = proplists:unfold(rebar_state:get(State, yang_opts, [])),
+    YangBuildDir = proplists:get_value(yang_build_dir, Opts, ?YANGBUILDDIR),
+    rebar_file_utils:delete_each(filelib:wildcard(filename:join([YangBuildDir, "*.hrl"]))).
+
+
+-spec format_error(any()) -> iolist().
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
+
+%% ===================================================================
+%% Internal functions 
+%% ===================================================================
